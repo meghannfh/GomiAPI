639a21c99fe7c92df70e9144dad10b92f1cc9f21
meghann <meghannfh@gmail.com>
2022-07-07 15:24:55 +0900

    mongo server connection was deleted

--- server.js
+++ server.js
@@ -16,28 +16,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let db;
-(dbName = "garbage-disposal-api"), (dbConnectionStr = process.env.MONGO_DB_KEY);
+(dbName = "garbage-disposal-api"), (dbConnectionStr = 'mongodb+srv://gomisorter:LJpMVxeGPYsSAGfg@cluster0.nf9ipwl.mongodb.net/?retryWrites=true&w=majority');
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+.then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db("garbage-disposal-api");
+  }
+);
+
+app.get("/", (req, res) => {
+  res.render("index.ejs")
+});
 
 app.get("/search", async (request, response) => {
   try {
     // console.log(req.query);
-    let result = await db
-      .collection("nagai-city-data")
-      .aggregate([
+    let result = await db.collection("nagai-city-data").aggregate([
         {
-          $search: {
-            autocomplete: {
-              query: `${req.query.query}`,
-              path: "name",
-              fuzzy: {
-                maxEdits: 2,
-                prefixLength: 2,
-              },
-            },
-          },
-        },
-      ])
-      .toArray();
+          "$search": {
+            "autocomplete": {
+              "query": `${req.query.query}`,
+              "path": "name",
+              "fuzzy": {
+                "maxEdits": 2,
+                "prefixLength": 2
+              }
+            }
+          }
+        }
+      ]).toArray();
 
     // let test = await db
     //   .collection("nagai-city-data")
@@ -52,9 +61,9 @@ app.get("/search", async (request, response) => {
 
 app.get("/get/:id", async (request, response) => {
   try {
-    console.log(req.params);
+    console.log(request.params);
     let result = await db.collection("nagai-city-data").findOne({
-      _id: ObjectId(req.params.id),
+      _id: ObjectId(request.params.id)
     });
     console.log(result);
     res.send(result);
